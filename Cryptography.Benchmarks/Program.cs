951a66612376c49cbeb0a731fbab3619ff13bbb4
Nemanja Mijailovic <metalnem@mijailovic.net>
2018-10-03 09:28:29 +0200

    Add benchmark project

---
+++ Cryptography.Benchmarks/Program.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace Cryptography.Benchmarks
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			BenchmarkRunner.Run<PolyvalBenchmark>();
+		}
+	}
+}
