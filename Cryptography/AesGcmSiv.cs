a664db4c5ebebb71ef90554214efbe78f18ed81f
Nemanja Mijailovic <metalnem@mijailovic.net>
2018-10-03 10:04:42 +0200

    Implement InitPowersTable methods

--- Cryptography/AesGcmSiv.cs
+++ Cryptography/AesGcmSiv.cs
@@ -111,18 +111,88 @@ namespace Cryptography
 			}
 		}
 
+		public static void InitPowersTable8(byte[] powersTable, byte[] hashKey)
+		{
+			Vector128<ulong> tmp0, tmp1, tmp2, tmp3, tmp4, poly, t;
+
+			fixed (byte* powersTablePtr = powersTable)
+			fixed (byte* hashKeyPtr = hashKey)
+			{
+				poly = Sse.StaticCast<uint, ulong>(Sse2.SetVector128(0xc2000000, 0, 0, 1));
+				t = Sse.StaticCast<byte, ulong>(Sse2.LoadVector128(hashKeyPtr));
+				tmp0 = t;
+				Sse2.Store(powersTablePtr, Sse.StaticCast<ulong, byte>(t));
+
+				for (int i = 1; i < 8; ++i)
+				{
+					tmp1 = Pclmulqdq.CarrylessMultiply(t, tmp0, 0x00);
+					tmp4 = Pclmulqdq.CarrylessMultiply(t, tmp0, 0x11);
+					tmp2 = Pclmulqdq.CarrylessMultiply(t, tmp0, 0x10);
+					tmp3 = Pclmulqdq.CarrylessMultiply(t, tmp0, 0x01);
+					tmp2 = Sse2.Xor(tmp2, tmp3);
+					tmp3 = Sse2.ShiftLeftLogical128BitLane(tmp2, 8);
+					tmp2 = Sse2.ShiftRightLogical128BitLane(tmp2, 8);
+					tmp1 = Sse2.Xor(tmp3, tmp1);
+					tmp4 = Sse2.Xor(tmp4, tmp2);
+					tmp2 = Pclmulqdq.CarrylessMultiply(tmp1, poly, 0x10);
+					tmp3 = Sse.StaticCast<uint, ulong>(Sse2.Shuffle(Sse.StaticCast<ulong, uint>(tmp1), 78));
+					tmp1 = Sse2.Xor(tmp3, tmp2);
+					tmp2 = Pclmulqdq.CarrylessMultiply(tmp1, poly, 0x10);
+					tmp3 = Sse.StaticCast<uint, ulong>(Sse2.Shuffle(Sse.StaticCast<ulong, uint>(tmp1), 78));
+					tmp1 = Sse2.Xor(tmp3, tmp2);
+					t = Sse2.Xor(tmp4, tmp1);
+					Sse2.Store(&powersTablePtr[16 * i], Sse.StaticCast<ulong, byte>(t));
+				}
+			}
+		}
+
+		public static void InitPowersTable6(byte[] powersTable, byte[] hashKey)
+		{
+			Vector128<ulong> tmp0, tmp1, tmp2, tmp3, tmp4, poly, t;
+
+			fixed (byte* powersTablePtr = powersTable)
+			fixed (byte* hashKeyPtr = hashKey)
+			{
+				poly = Sse.StaticCast<uint, ulong>(Sse2.SetVector128(0xc2000000, 0, 0, 1));
+				t = Sse.StaticCast<byte, ulong>(Sse2.LoadVector128(hashKeyPtr));
+				tmp0 = t;
+				Sse2.Store(powersTablePtr, Sse.StaticCast<ulong, byte>(t));
+
+				for (int i = 1; i < 6; ++i)
+				{
+					tmp1 = Pclmulqdq.CarrylessMultiply(t, tmp0, 0x00);
+					tmp4 = Pclmulqdq.CarrylessMultiply(t, tmp0, 0x11);
+					tmp2 = Pclmulqdq.CarrylessMultiply(t, tmp0, 0x10);
+					tmp3 = Pclmulqdq.CarrylessMultiply(t, tmp0, 0x01);
+					tmp2 = Sse2.Xor(tmp2, tmp3);
+					tmp3 = Sse2.ShiftLeftLogical128BitLane(tmp2, 8);
+					tmp2 = Sse2.ShiftRightLogical128BitLane(tmp2, 8);
+					tmp1 = Sse2.Xor(tmp3, tmp1);
+					tmp4 = Sse2.Xor(tmp4, tmp2);
+					tmp2 = Pclmulqdq.CarrylessMultiply(tmp1, poly, 0x10);
+					tmp3 = Sse.StaticCast<uint, ulong>(Sse2.Shuffle(Sse.StaticCast<ulong, uint>(tmp1), 78));
+					tmp1 = Sse2.Xor(tmp3, tmp2);
+					tmp2 = Pclmulqdq.CarrylessMultiply(tmp1, poly, 0x10);
+					tmp3 = Sse.StaticCast<uint, ulong>(Sse2.Shuffle(Sse.StaticCast<ulong, uint>(tmp1), 78));
+					tmp1 = Sse2.Xor(tmp3, tmp2);
+					t = Sse2.Xor(tmp4, tmp1);
+					Sse2.Store(&powersTablePtr[16 * i], Sse.StaticCast<ulong, byte>(t));
+				}
+			}
+		}
+
 		public static void PolyvalHorner(byte[] tag, byte[] hashKey, byte[] input)
 		{
 			int length = input.Length / 16;
-			Vector128<ulong> tmp1, tmp2, tmp3, tmp4, t, h, poly;
+			Vector128<ulong> tmp1, tmp2, tmp3, tmp4, poly, t, h;
 
 			fixed (byte* tagPtr = tag)
 			fixed (byte* hashKeyPtr = hashKey)
 			fixed (byte* inputPtr = input)
 			{
+				poly = Sse.StaticCast<uint, ulong>(Sse2.SetVector128(0xc2000000, 0, 0, 1));
 				t = Sse.StaticCast<byte, ulong>(Sse2.LoadVector128(tagPtr));
 				h = Sse.StaticCast<byte, ulong>(Sse2.LoadVector128(hashKeyPtr));
-				poly = Sse.StaticCast<uint, ulong>(Sse2.SetVector128(0xc2000000, 0, 0, 1));
 
 				for (int i = 0; i < length; ++i)
 				{
