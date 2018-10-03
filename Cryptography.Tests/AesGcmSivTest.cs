ba0d1384514daee4b8e451a59ece7b4672e162a2
Nemanja Mijailovic <metalnem@mijailovic.net>
2018-10-03 10:18:33 +0200

    Add InitPowersTable tests

--- Cryptography.Tests/AesGcmSivTest.cs
+++ Cryptography.Tests/AesGcmSivTest.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using Newtonsoft.Json.Linq;
 using Xunit;
 
@@ -34,6 +35,38 @@ namespace Cryptography.Tests
 					Assert.Equal(polyval, Hex.Encode(tag));
 				}
 			}
+		}
+
+		[Fact]
+		public void TestInitPowersTable8() => TestInitPowersTable(8);
+
+		[Fact]
+		public void TestInitPowersTable6() => TestInitPowersTable(6);
+
+		private void TestInitPowersTable(int size)
+		{
+			var powersTable = new byte[size * 16];
+			var hashKey = Hex.Decode("d8733394680050b0610782116bed63c4");
+
+			switch (size)
+			{
+				case 8: AesGcmSiv.InitPowersTable8(powersTable, hashKey); break;
+				case 6: AesGcmSiv.InitPowersTable6(powersTable, hashKey); break;
+			}
+
+			var powers = new byte[size][];
+			powers[0] = hashKey;
+
+			for (int i = 1; i < size; ++i)
+			{
+				powers[i] = new byte[16];
+				AesGcmSiv.PolyvalHorner(powers[i], hashKey, powers[i - 1]);
+			}
+
+			var expected = Hex.Encode(powersTable);
+			var actual = Hex.Encode(powers.SelectMany(b => b).ToArray());
+
+			Assert.Equal(expected, actual);
 		}
 
 		private static string GetString(JToken token, string property)
